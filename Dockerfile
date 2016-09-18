e68009a464eb94a0d48f4bb0f7a4571f33f2e561
Mikael <mikael.snellman@cs.helsinki.fi>
2016-09-18 14:11:36 +0300

    UI changes

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+# Build myapp server Docker container
+FROM coreos/apache
+COPY dist /var/www/
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,4 +32,9 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .controller('HeaderCtrl', function ($scope, $location) { 
+    $scope.isActive = function (viewLocation) { 
+      return viewLocation === $location.path();
+    };
   });
